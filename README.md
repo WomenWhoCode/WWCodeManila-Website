28e493e393fbe984fb001944aae495d81dd46be6
michie <veryhappymichie@gmail.com>
2017-10-19 01:58:20 +0800

    Move site repo from Bitbucket to Github

---
+++ README.md
@@ -0,0 +1,8 @@
+# README #
+
+This is for our Official Hackathon Website. (Private repo)
+
+Our assets are hosted at: https://bitbucket.org/wwcodemanila/hackathon-assets
+Please keep it public. Thanks.
+
+TODO: https://bitbucket.org/wwcodemanila/hackathon-website/addon/bitbucket-trello-addon/trello-board
---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const ejsLayouts = require('express-ejs-layouts')
+
+const app = express()
+
+const port = process.env.PORT
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res) {
+	const organizers = [
+		{	
+			id: 1,
+			name: "Michie Ang",
+			imageURL: "img/organizers/michie.png"
+		},
+		{	
+			id: 2,
+			name: "Joy Paas",
+			imageURL: "img/organizers/joy.jpg"
+		},
+		{	
+			id: 3,
+			name: "Steffi Tan",
+			imageURL: "img/organizers/steffi.jpg"	
+		},
+		{	
+			id: 4,
+			name: "Joseph Baldemor",
+			imageURL: "img/organizers/joseph.jpg"
+		},
+		{	
+			id: 5,
+			name: "Jeykle Sunas",
+			imageURL: "img/organizers/jeykle.png"
+		},
+		{	
+			id: 6,
+			name: "Micaela Reyes",
+			imageURL: "img/organizers/micaela.jpg"
+		},
+		{	
+			id: 7,
+			name: "Mars Gabutero",
+			imageURL: "img/organizers/mars.jpg"
+		}] 
+
+	const mentors = [
+		{	
+			id: 1,
+			name: "You",
+			imageURL: "https://www.shareicon.net/data/512x512/2015/10/04/112038_glasses_512x512.png"
+		}]
+
+    res.render('index', { organizers: organizers, mentors: mentors })
+})
+
+app.get('/participants', function(req, res) {
+    res.render('participants')
+})
+
+app.get('/supporters', function(req, res) {
+    res.render('supporters')
+})
+
+app.get('/travel', function(req, res) {
+    res.render('travel')
+})
+
+app.get('/coc', function(req, res) {
+    res.render('coc')
+})
+
+app.get('/faq', function(req, res) {
+    res.render('faq')
+})
+
+app.get(['/hackathon', '/join'], function(req, res) {
+    res.render('join')
+})
+
+app.listen(port)
