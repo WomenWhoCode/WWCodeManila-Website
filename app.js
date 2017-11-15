6dc4d248a71d12df1bfb7cf02436d379ad7e0c6e
amdg <amdg@users.noreply.github.com>
2017-11-16 00:28:26 +0530

    Whitespace fixes

--- app.js
+++ app.js
@@ -27,13 +27,13 @@ firebase.initializeApp({
 const authenticated = function(req, res, next) {
   if (req.session.user) {
     firebase.auth().getUser(req.session.user)
-    .then((record) => {
-      req.user = record
-      next()
-    })
-    .catch((error) => {
-      next(error)
-    })
+      .then((record) => {
+        req.user = record
+        next()
+      })
+      .catch((error) => {
+        next(error)
+      })
   } else {
     res.redirect('/login')
   }
@@ -76,49 +76,49 @@ app.use(session({
 
 app.get('/', function(req, res) {
   const organizers = [
-    {	
+    {
       id: 1,
       name: "Michie Ang",
       imageURL: "img/organizers/michie.png"
     },
-    {	
+    {
       id: 2,
       name: "Joy Paas",
       imageURL: "img/organizers/joy.jpg"
     },
-    {	
+    {
       id: 3,
       name: "Steffi Tan",
-      imageURL: "img/organizers/steffi.jpg"	
+      imageURL: "img/organizers/steffi.jpg"
     },
-    {	
+    {
       id: 4,
       name: "Joseph Baldemor",
       imageURL: "img/organizers/joseph.jpg"
     },
-    {	
+    {
       id: 5,
       name: "Jeykle Sunas",
       imageURL: "img/organizers/jeykle.png"
     },
-    {	
+    {
       id: 6,
       name: "Micaela Reyes",
       imageURL: "img/organizers/micaela.jpg"
     },
-    {	
+    {
       id: 7,
       name: "Mars Gabutero",
       imageURL: "img/organizers/mars.jpg"
     },
-    {	
+    {
       id: 8,
       name: "Clau Yagyagan",
       imageURL: "img/organizers/clau.png"
-    }] 
+    }]
 
   const mentors = [
-    {	
+    {
       id: 1,
       name: "You",
       imageURL: "https://www.shareicon.net/data/512x512/2015/10/04/112038_glasses_512x512.png"
@@ -134,7 +134,7 @@ app.get('/participants', function(req, res) {
 app.get('/supporters', function(req, res) {
 
   const platinum_advocate = [
-    {	
+    {
       id: 1,
       name: "Atlassian",
       url: "https://atlassian.com",
@@ -142,7 +142,7 @@ app.get('/supporters', function(req, res) {
     }]
 
   const inspire = [
-    {	
+    {
       id: 1,
       name: "Accenture",
       url: "https://accenture.com",
@@ -150,13 +150,13 @@ app.get('/supporters', function(req, res) {
     }]
 
   const nourish = [
-    {	
+    {
       id: 1,
       name: "Zendesk",
       url: "https://zendesk.com",
       imageURL: "img/supporters/company/zendesk-125.png"
     },
-    {	
+    {
       id: 2,
       name: "Migo",
       url: "https://www.migo.tv",
@@ -164,7 +164,7 @@ app.get('/supporters', function(req, res) {
     }]
 
   const sustain = [
-    {	
+    {
       id: 1,
       name: "You",
       url: "https://womenwhocode.com",
@@ -172,7 +172,7 @@ app.get('/supporters', function(req, res) {
     }]
 
   const support = [
-    {	
+    {
       id: 1,
       name: "You",
       url: "https://womenwhocode.com",
@@ -180,7 +180,7 @@ app.get('/supporters', function(req, res) {
     }]
 
   const kiddie_hackathon = [
-    {	
+    {
       id: 1,
       name: "Power Mac Center",
       url: "http://www.powermaccenter.com",
@@ -188,13 +188,13 @@ app.get('/supporters', function(req, res) {
     }]
 
   const media_partners = [
-    {	
+    {
       id: 1,
       name: "Manila Bulletin",
       url: "https://mb.com.ph",
       imageURL: "img/supporters/media/mb_logo.jpg"
     },
-    {	
+    {
       id: 2,
       name: "e27",
       url: "https://e27.co",
@@ -202,55 +202,55 @@ app.get('/supporters', function(req, res) {
     }]
 
   const community_partners = [
-    {	
+    {
       id: 1,
       name: "PyLadies",
       url: "https://www.meetup.com/pyladiesmanila/",
       imageURL: "img/supporters/community/PyLadies.jpg"
     },
-    {	
+    {
       id: 2,
       name: "Coding Girls",
       url: "https://www.facebook.com/coding.girls.manila//",
       imageURL: "img/supporters/community/coding_girls.png"
     },
-    {	
+    {
       id: 3,
       name: "PythonPH",
       url: "https://python.ph/",
       imageURL: "img/supporters/community/PythonPH.png"
     },
-    {	
+    {
       id: 4,
       name: "VRPH",
       url: "https://www.facebook.com/vrphilippines/",
       imageURL: "img/supporters/community/VRPH_Logo.png"
     },
-    {	
+    {
       id: 5,
       name: "SwiftPH",
       url: "http://Swift.PH",
       imageURL: "img/supporters/community/SwiftPH_Logo.png"
     },
-    {	
+    {
       id: 6,
       name: "Philippine Android Developer Community (PADC)",
       url: "http://facebook.com/padcsummit/",
       imageURL: "img/supporters/community/PADC.png"
     },
-    {	
+    {
       id: 7,
       name: "Philippine Web Designer (PWDO)",
       url: "http://facebook.com/padcsummit/",
       imageURL: "img/supporters/community/pwdo.png"
     },
-    {	
+    {
       id: 8,
       name: "Drupal Philippines",
       url: "https://groups.drupal.org/philippines",
       imageURL: "img/supporters/community/Drupal.png"
     },
-    {	
+    {
       id: 9,
       name: "LaravelPH",
       url: "https://www.facebook.com/laravelph",
@@ -259,7 +259,7 @@ app.get('/supporters', function(req, res) {
   ]
 
   const special_thanks = [
-    {	
+    {
       id: 1,
       name: "Pez.ai",
       url: "https://pez.ai",
